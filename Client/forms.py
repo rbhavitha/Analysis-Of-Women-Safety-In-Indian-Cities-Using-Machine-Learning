3dc7d18a4b7d0f25d6febbbcbbc5ee55775aca3e
rbhavitha <71348485+rbhavitha@users.noreply.github.com>
2021-11-04 15:36:38 +0530

    Delete Client directory

--- Client/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from Client.models import Userregister_Model
-
-
-class Userregister_Form(forms.ModelForm):
-    class Meta:
-        model = Userregister_Model
-        fields = ('name','email','password','phoneno', 'address', 'dob', 'country','state','city')
